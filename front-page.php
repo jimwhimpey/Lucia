37b40439e252d9c6a5239d9249819a01cf7fb12f
Jim Whimpey <jimwhimpey@me.com>
2011-12-09 22:02:16 +1000

    Got the posts onto the homepage. Setup the columns

--- front-page.php
+++ front-page.php
@@ -1,14 +1,30 @@
 <?php get_header(); ?>
 
 	<div class="blurb"><p>We are a bicycle racing club based at the University of Queensland in Brisbane, Australia. 
-			Our club has a reputation as one of Australia’s leading clubs and has produced a number of Olympic, 
-			Commonwealth and National Champions.</p></div>
+			Our club has a reputation as one of Australia’s leading clubs and has produced a number of National, Commonwealth, 
+			and Olympic Champions.</p></div>
 
 	<div class="main">
+		
+		<div class="content">
+			
+			<h2>Latest Club News</h2>
+			
+			
+			<?php
 
-		<?php while ( have_posts() ) : the_post(); ?>
+  $args = array(
+    'orderby' => 'date',
+    'order' => 'DESC',
+    'posts_per_page' => 10
+  );
+  $temp = $wp_query;  // assign orginal query to temp variable for later use   
+  $wp_query = null;
+  $wp_query = new WP_Query($args); 
+  if( have_posts() ) { 
+		while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
 
-			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 				
 				<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 
@@ -29,17 +45,33 @@
 				
 			</div>
 
-			<?php comments_template( '', true ); ?>
 
-		<?php endwhile;  ?>
+    <?php endwhile; ?>
 
+<?php
+}	
+	$wp_query = $temp;  //reset back to original query
+	
 
-		<?php if (  $wp_query->max_num_pages > 1 ) : ?>
-			<div id="nav-below" class="navigation">
-				<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'twentyten' ) ); ?></div>
-				<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?></div>
-			</div>
-		<?php endif; ?>
+?>
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		</div>
+		
+		<div class="sidebar">
+			
+			
+		</div>
+
 
 	</div>
 
