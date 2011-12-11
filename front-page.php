a60d709c2591078dff71dcba8b7fdab23f719caf
Jim Whimpey <jimwhimpey@me.com>
2011-12-11 15:39:29 +1000

    Finished basic styling of the news on the homepage

--- front-page.php
+++ front-page.php
@@ -6,68 +6,57 @@
 
 	<div class="main">
 		
-		<div class="content">
+		<div class="blog col-a content">
 			
 			<h2>Latest Club News</h2>
 			
-			
 			<?php
 
-  $args = array(
-    'orderby' => 'date',
-    'order' => 'DESC',
-    'posts_per_page' => 10
-  );
-  $temp = $wp_query;  // assign orginal query to temp variable for later use   
-  $wp_query = null;
-  $wp_query = new WP_Query($args); 
-  if( have_posts() ) { 
-		while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
-
-<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				$args = array(
+				  'orderby' => 'date',
+				  'order' => 'DESC',
+				  'posts_per_page' => 10
+				);
+				$temp = $wp_query;  // assign orginal query to temp variable for later use   
+				$wp_query = null;
+				$wp_query = new WP_Query($args); 
 				
-				<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+  				if( have_posts() ) { 
+					while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
 
-				<div class="entry-content">
-					<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?>
-					<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentyten' ), 'after' => '</div>' ) ); ?>
-				</div>
-
-				<div class="entry-utility">
-					<?php if ( count( get_the_category() ) ) : ?>
-						<span class="cat-links">
-							<?php printf( __( '<span class="%1$s">Posted in</span> %2$s', 'twentyten' ), 'entry-utility-prep entry-utility-prep-cat-links', get_the_category_list( ', ' ) ); ?>
-						</span>
-					<?php endif; ?>
-					<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'twentyten' ), __( '1 Comment', 'twentyten' ), __( '% Comments', 'twentyten' ) ); ?></span>
-					<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="meta-sep">|</span> <span class="edit-link">', '</span>' ); ?>
-				</div>
+						<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 				
+							<h3 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
+
+							<div class="entry-content">
+								<?php global $more; $more = 0; the_content("More..."); ?>
+							</div>
+
+							<div class="meta">
+								<?php if ( count( get_the_category() ) ) : ?>
+									<span class="cat-links">
+										<?php printf( __( '<span class="%1$s">Posted in</span> %2$s', 'twentyten' ), 'entry-utility-prep entry-utility-prep-cat-links', get_the_category_list( ', ' ) ); ?>
+									</span>
+								<?php endif; ?>
+								by <?php the_author(); ?> 
+								| <span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'twentyten' ), __( '1 Comment', 'twentyten' ), __( '% Comments', 'twentyten' ) ); ?></span>
+							</div>
+				
+						</div>
+
+    		<?php 
+					endwhile;
+				}	
+				$wp_query = $temp;  //reset back to original query
+			?>
+			
+			<div class="post-nav">
+				<div class="older"><a href="<?php bloginfo('url'); ?>/news"><span>&larr;</span> Older News</a></div>
 			</div>
-
-
-    <?php endwhile; ?>
-
-<?php
-}	
-	$wp_query = $temp;  //reset back to original query
-	
-
-?>
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
 			
 		</div>
 		
-		<div class="sidebar">
+		<div class="col-b events">
 			
 			
 		</div>
