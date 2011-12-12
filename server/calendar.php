34868727e81cc086a95031ee179224471991e42b
Jim Whimpey <jimwhimpey@me.com>
2011-12-13 07:41:45 +1000

    Switched the calendar stuff to be server side. No idea why I didn't do this from the beginning, I had to jump through a lot of hoops to make it client side. I think I'm stuck in this weird everything has to be server side mode

--- server/calendar.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-	// Takes URL, gets the result and returns it
-	$contents = file_get_contents(urldecode($_GET["url"]));
-	echo $contents;
-
-?>
--- utility-calendar.php
+++ utility-calendar.php
@@ -1,6 +1,48 @@
 <div class="col-b events">
 	<h2>Upcoming Events</h2>
-	<div class="loading"></div>
-	<table></table>
+	<table>
+		
+		<?php
+	
+			// Set the timezone
+			date_default_timezone_set("Australia/Brisbane");
+	
+			// Get the Calendar data from Google
+			$contents = file_get_contents("https://www.googleapis.com/calendar/v3/calendars/rabts4o5f0hl2aema0sqsuot78@group.calendar.google.com/events?maxResults=10&timeMin=2011-12-11T01:00:00.000Z&pp=1&key=AIzaSyAQowsWAInPNbY5sMQ9HrWzIBlyeF7pRtI&orderBy=startTime&singleEvents=true");
+			$json = json_decode($contents);
+		
+			// Loop through the results
+			foreach ($json->items as $item) {
+			
+				// Process the date
+				if (empty($item->start->dateTime)) {
+					$time = false;
+					$eventDate = strtotime($item->start->date);
+				} else {
+					$time = true;
+					$eventDate = strtotime($item->start->dateTime);
+				}
+			
+				// Create the table rows
+				$row = "<tr>";
+				$row .= "<td class='summary'><span class='name'>" . $item->summary . "</span>";
+				if (!empty($item->location)) {
+					$row .= "<span class='location'>" . $item->location . "</span>";
+				}
+				$row .= "</td>";
+				$row .= "<td class='date'><span class='day'>" . date("jS M", $eventDate) . "</span>";
+				if ($time) {
+					$row .= "<span class='time'>" . date("g:iA", $eventDate) . "</span></td>";
+				}
+				$row .= "</tr>";
+			
+				// Output the row
+				echo $row;
+			
+			}
+	
+		?>
+		
+	</table>
 	<p class="more"><a href="<?php bloginfo('url'); ?>/riding-racing/calendar/">See the full calendar &rarr;</a></p>
 </div>
