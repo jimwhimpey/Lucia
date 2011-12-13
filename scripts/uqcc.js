edc69cab32ce09c84d17cb8999bdd918f77af752
Jim Whimpey <jimwhimpey@me.com>
2011-12-13 18:19:20 +1000

    Finished adding Flickr photos

--- scripts/uqcc.js
+++ scripts/uqcc.js
@@ -1,4 +1,5 @@
 $(function(){
 
+	$(".flickr ul li:last-of-type").css("margin", "0");
 
 });
--- utility-flickr.php
+++ utility-flickr.php
@@ -7,14 +7,27 @@
 		
 			// Get the Calendar data from Flickr
 			$contents = file_get_contents("http://api.flickr.com/services/feeds/photos_public.gne?id=42612547@N03&lang=en-us&format=json");
-			$contents = preg_replace('/.+?({.+}).+/','$1',$contents);
+			$contents = preg_replace('/^jsonFlickrFeed\(|\)$/', '', $contents);
 			$json = json_decode($contents);
 
-			echo "<pre>";
-			print_r($json);
-			echo "</pre>";
+			// Loop through the items
+			for ($i=0; $i < 5; $i++) { 
+				
+				// Start creating the HTML
+				$html = "<li>";
+				$html .= "<a href='" . $json->items[$i]->link . "'>";
+				$html .= "<img src='" . $json->items[$i]->media->m . "' width='178' />";
+				$html .= "</a>";
+				$html .= "</li>";
+				
+				// Put it into the page
+				echo $html;
+				
+			}
 		
 		?>
+		
+		<div style="clear: left;"></div>
 		
 	</ul>
 	
