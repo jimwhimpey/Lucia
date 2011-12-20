0883d270d753672a94e1d364038d717439967980
Jim Whimpey <jimwhimpey@me.com>
2011-12-20 20:05:17 +1000

    Styled the subpages sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,6 @@
 <?php
 
-	if (is_page('racing-results')) {
+	if (is_page('racing')) {
 		get_template_part('utility-subpages');
 	} else {
 		get_template_part('utility-calendar');
--- utility-subpages.php
+++ utility-subpages.php
@@ -1,4 +1,6 @@
 <div class="col-b subpages">
 	<h2>Race Results</h2>
-	<?php wp_list_pages(array('title_li' => '', 'child_of'  => $post->ID)); ?>
+	<ul>
+		<?php wp_list_pages(array('title_li' => '', 'child_of'  => $post->ID)); ?>
+	</ul>
 </div>
