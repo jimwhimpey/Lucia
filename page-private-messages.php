c54077db6808c7f71845bc8c22697ef2698f2171
Jim Whimpey <jimwhimpey@me.com>
2012-01-23 20:32:48 +1000

    Finished the private messaging stuff! So many modifications made to the private messaging plugin

--- page-private-messages.php
+++ page-private-messages.php
@@ -5,47 +5,34 @@
 ?>
 
 <?php get_header(); ?>
-
-	<script type="text/javascript">
-		// Switch between send page, inbox and outbox
-		function pmSwitch(page) {
-			document.getElementById('pm-send').style.display = 'none';
-			document.getElementById('pm-inbox').style.display = 'none';
-			document.getElementById('pm-outbox').style.display = 'none';
-			document.getElementById(page).style.display = '';
-			return false;
-		}
-	</script>
-	<script type="text/javascript" src="<?php echo WP_PLUGIN_URL; ?>/private-messages-for-wordpress/js/jquery.min.js"></script>
-	<script type="text/javascript" src="<?php echo WP_PLUGIN_URL; ?>/private-messages-for-wordpress/js/jquery.autoSuggest.packed.js"></script>
-	<script type="text/javascript" src="<?php echo WP_PLUGIN_URL; ?>/private-messages-for-wordpress/js/script.js"></script>
-	<link rel="stylesheet" type="text/css" href="<?php echo WP_PLUGIN_URL; ?>/private-messages-for-wordpress/css/style.css" />
 	
-	
-
 	<div class="main">
 
 		<div class="page post col-a content private-messages">
 
-				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<div id="" <?php post_class(); ?>>
 				
 					<h2><?php the_title(); ?></h3>
-						
-					<a href="javascript:void(0);" onclick="pmSwitch('pm-send');">Send</a> | <a href="javascript:void(0);" onclick="pmSwitch('pm-inbox');">Inbox</a> | <a href="javascript:void(0);" onclick="pmSwitch('pm-outbox');">Outbox</a>					
 					
-					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-							<?php
-							$show = array(true, false, false);
-							if (isset($_REQUEST['page']) && $_REQUEST['page'] == 'rwpm_inbox') {
-								$show = array(false, true, false);
-							} elseif (isset($_REQUEST['page']) && $_REQUEST['page'] == 'rwpm_outbox') {
-								$show = array(false, false, true);
-							}
-							?>
-							<div id="pm-send" <?php if (!$show[0]) echo 'style="display:none"'; ?>><?php rwpm_send();?></div>
-							<div id="pm-inbox" <?php if (!$show[1]) echo 'style="display:none"'; ?>><?php rwpm_inbox();?></div>
-							<div id="pm-outbox" <?php if (!$show[2]) echo 'style="display:none"'; ?>><?php rwpm_outbox();?></div>
-					<?php endwhile; endif; ?>
+					<div class="entry-content">
+					
+						<a href="?">Send</a> | <a href="?page=rwpm_inbox">Inbox</a> | <a href="?page=rwpm_outbox">Outbox</a>					
+					
+						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+								<?php
+								$show = array(true, false, false);
+								if (isset($_REQUEST['page']) && $_REQUEST['page'] == 'rwpm_inbox') {
+									$show = array(false, true, false);
+								} elseif (isset($_REQUEST['page']) && $_REQUEST['page'] == 'rwpm_outbox') {
+									$show = array(false, false, true);
+								}
+								?>
+								<div id="pm-send" <?php if (!$show[0]) echo 'style="display:none"'; ?>><?php rwpm_send();?></div>
+								<div id="pm-inbox" <?php if (!$show[1]) echo 'style="display:none"'; ?>><?php rwpm_inbox();?></div>
+								<div id="pm-outbox" <?php if (!$show[2]) echo 'style="display:none"'; ?>><?php rwpm_outbox();?></div>
+						<?php endwhile; endif; ?>
+						
+					</div>
 				
 				</div>			
 		
--- utility-tools.php
+++ utility-tools.php
@@ -2,8 +2,15 @@
 	<?php if(is_user_logged_in()) { ?>
 		<?php global $current_user; ?>
 		<p>Logged in as <a href="<?php echo home_url() . '/forum/users/' . $current_user->user_login . '/'; ?>"><?php echo $current_user->display_name; ?></a></p>
+		
+		<?php
+			// Working out how many unread messages
+			$msgs = $wpdb->get_results( 'SELECT `id`, `sender`, `subject`, `read`, `date` FROM ' . $wpdb->prefix . 'pm WHERE `recipient` = "' . $current_user->user_login . '" AND `deleted` != "2" AND `read` = "0" ORDER BY `date` DESC' );
+			$unread = count($msgs);
+		?>
+		
 		<ul>
-			<li><a href="">Private messages</a></li>
+			<li><a href="<?php echo home_url() . '/private-messages/?page=rwpm_inbox' ?>">Private messages <?php if ($unread > 0) { echo "<span>(" . $unread . " New)</span>"; } ?></a></li> 
 			<li><a href="<?php echo home_url() . '/forum/users/' . $current_user->user_login . '/edit/'; ?>">Edit profile</a></li>
 			<li><a href="<?php echo wp_logout_url( home_url() ); ?>">Logout</a></li>
 		</ul>
