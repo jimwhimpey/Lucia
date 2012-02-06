41c30e7db55be3c96ce67ed928890cba3df4d649
Jim Whimpey <jimwhimpey@me.com>
2012-02-07 07:42:47 +1000

    Added bug report link and admin link to the footer

--- footer.php
+++ footer.php
@@ -1,6 +1,29 @@
-		<!--<div class="footer">
-			Site by <a href="http://jimwhimpey.com">Jim Whimpey</a>
-		</div><!-- #footer -->
+		<div class="footer">
+			Submit bugs to <script language="JavaScript" type="text/javascript">
+//<!--
+
+function hiveware_enkoder(){var i,j,x,y,x=
+"x=\"z?$2z6?.^$|(~:,955@897l8<*+8@+.gl9>3kg:6u96hr69sw7jdq5h8o6i:48>::f*61v" +
+"{9?6l5>988*i:h@8vq19{x@9.9|7~7,:3d4@@9..l5>9k<w%25j_q_h_o91|{9?~l8>,3@@6l{" +
+"+3u%25r_i:>5*6*@@:|@>{,]4,+su6w%25v@e{x^^v^$1?{z@f{y>h,@,m3>+:w6D3u5d7k%25" +
+"f_1|{:+%22o5d5y;h;@9m5>>%259>,,8m,+6u8.9i9l869>5w:v/e8r7c9e:u7g<plw3?+z6=u" +
+"):)9?h{5=6^^5^$w>6,vm8+:u5w:v5e:xev51x|6@v|f%22h>8,58:/7l1+6u{w7v7e9x:v81h" +
+"{.@4.*|8~3,@3446@9.glg>lk+wujrqih9o91i{5?9l>>*8*@@l|+5u6rfi9%228>>,,84/+lu" +
+"+hugw5v6e8xw*vgefxqvE9t9c9jfE8o1q{t@h{0>i9p9k6t5v9U,?,-3{+=w6:;<?;-:l9+D4u" +
+"5d>klf*6h3k9=534/1+{k+*ovdC4g:f9q8E3t4c@jle+0uz|?7l>};+%25-_-rki=%22j>v,i_" +
+"p_g9ne06z6>4k/=l2+?uk9*3t8q6h9=w+vze*xgv{e!7+6l:^$<=1{{?@).)|=dh5q6t5*ik~?" +
+",25=@k.>5z90hn7g6pli>vkjw=jk:-4?f:989+q}hhoq1t{*il<?5O9cfv?jl0>oxkvpg*kz90" +
+"wnig1p|i@v|j%22.jk9-q:68h+>=,/4//ll@9?okh=1+5}+{u-w?vze0{e8j?c9tlCxvv*1l{+" +
+"@=f!>!9{3=$=l?gxcn*z0ejctCv*2++=z?z0uwduvt*3+={?))=hqt*k?2=k>z0ngpivj=k-?4" +
+"+}{-?z0uwduvt*k.3+=!hqt*k?3=k>z0ngpivj=k-?4+}{-?z0uwduvt*k.3+=!{?{0uwduvt*" +
+"l+=\";y='';x=unescape(x);for(i=0;i<x.length;i++){j=x.charCodeAt(i)-2;if(j<" +
+"32)j+=94;y+=String.fromCharCode(j)}y";
+while(x=eval(x));}hiveware_enkoder();
+
+//-->
+</script>
+		<?php if (current_user_can('edit_pages')) { echo "<br /><a href='" . admin_url() . "'>Administration</a>"; } ?>
+		</div>
 		<?php wp_footer(); ?>
 	</body>
 </html>
