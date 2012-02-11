07fd796275a9efd5fcde892a81cc6ff79af6d000
Jim Whimpey <jimwhimpey@me.com>
2012-02-11 11:59:39 +1000

    Fixed the forum heading when not logged in

--- archive-forum.php
+++ archive-forum.php
@@ -22,37 +22,40 @@
 			
 			<?php if (!is_user_logged_in()) { // Not logged in ?>
 				
-				<h2>Forum</h2>
+				<div id="forum-front" class="bbp-forum-front">
 				
-				<div class="entry-content forum-logged-out">
+					<h2 class="entry-title">Forum</h2>
+				
+					<div class="entry-content forum-logged-out">
 					
-					<p>The forum is only open to UQCC members.</p>
+						<p>The forum is only open to UQCC members.</p>
 					
 					
-					<h3>Login</h3>
+						<h3>Login</h3>
 					
-					<p>Your username will always be first name followed by surname even if you've changed 
-						your display name.</p>
+						<p>Your username will always be first name followed by surname even if you've changed 
+							your display name.</p>
 						
-					<?php wp_login_form(array('remember' => true)); ?> 
+						<?php wp_login_form(array('remember' => true)); ?> 
 					
 					
-					<h3>If This Is Your First Time Logging In...</h3>
+						<h3>If This Is Your First Time Logging In...</h3>
 					
-					<p>Use your first name followed by your surname as your username (eg. jimwhimpey) as your username 
-					and your date of birth in this format YYYYMMDD (eg. 19871205) as your password. Once inside you can 
-					change both your display name and password.</p>
-					
-					
-					<h3>If You're Not a Club Member...</h3>
-					
-					<p>Look into <a href="../how-to-join/">how to join.</a></p>
+						<p>Use your first name followed by your surname as your username (eg. jimwhimpey) as your username 
+						and your date of birth in this format YYYYMMDD (eg. 19871205) as your password. Once inside you can 
+						change both your display name and password.</p>
 					
 					
-					<h3>If You've Forgotten Your Password...</h3>
+						<h3>If You're Not a Club Member...</h3>
 					
-					<p><a href="<?php bloginfo('url'); ?>/wp-login.php?action=lostpassword">Use this form</a>.</p>
+						<p>Look into <a href="../how-to-join/">how to join.</a></p>
 					
+					
+						<h3>If You've Forgotten Your Password...</h3>
+					
+						<p><a href="<?php bloginfo('url'); ?>/wp-login.php?action=lostpassword">Use this form</a>.</p>					
+					
+					</div>
 					
 				</div>
 				
