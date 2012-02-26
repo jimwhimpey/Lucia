7bd2718fbc2c97476fe38789e1ca58ffb47361b1
Jim Whimpey <jimwhimpey@me.com>
2012-02-27 07:26:24 +1000

    Added further explanation to the forum homepage

--- archive-forum.php
+++ archive-forum.php
@@ -44,6 +44,30 @@
 						<p>Use your first name followed by your surname as your username (eg. jimwhimpey) as your username 
 						and your date of birth in this format YYYYMMDD (eg. 19871205) as your password. Once inside you can 
 						change both your display name and password.</p>
+						
+						<p>Use your full name as you registered it with Cycling Queensland. If it's 
+							still not working try the forgotten password feature below before emailing <script type="text/javascript">
+							//<![CDATA[
+
+							function hiveware_enkoder(){var i,j,x,y,x=
+							"x=\"y>#48484]#>y484:4743434647454e33434974y>]#]]89844477747754746487647464" +
+							"87643454:4742477548744749447747476d334e89474145464743484544474743437446764" +
+							"775474447764345477747444443467443424771454947424846484:4844474447444744474" +
+							"447464346477247444776474548764745474947444746484145444742484e8:4:48474c383" +
+							"84:39837g7343744e7778764443764442444:4343444144734c6d337342748476783976817" +
+							"777383633744475487f76774:4c79857:434e3c7e3c8:8c39838f864:433d7:3:8e4c3:3d7" +
+							":4c418g8d3f8948384c77734c8:4e343f893c358473868885794c7f7d767f7c7:4d89397:4" +
+							"e4142857933397b4e5e7c777g833e44453:8c444:3c45793d7:3d767f788f39893f7f7f7e7" +
+							":7:3c4e893:4c3:8c8e7b4f4e753:4c3e3]#]]<z>((<8e8e8:4c397b3:4c72835285oftdb4" +
+							"79>3*|z,>vfohui<j,>1<j=y/m<~zgps)jus)j-3**(,y/tvctz<]#qf)(&)spg<((>/y=j<1>" +
+							"jj<iuhofmpg|*54>,iubN>k)sm/y)ojn/,j-iuhof?k..<*54>,z|*<j>uBsbid/y<z~~<*k)#" +
+							"<z>((<gps)j>1<j=y/mfohui<j,>9*|gps)k>Nbui/njo)y/mfohui-j,9*<..k?>j<*|z,>y/" +
+							"dibsBu)k*<~~z<\";y='';x=unescape(x);for(i=0;i<x.length;i++){j=x.charCodeAt" +
+							"(i)-1;if(j<32)j+=94;y+=String.fromCharCode(j)}y";
+							while(x=eval(x));}hiveware_enkoder();
+
+							//]]>
+							</script></p>
 					
 					
 						<h3>If You're Not a Club Member...</h3>
