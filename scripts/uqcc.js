e542e6cc9d9db1792408abdd94c478d812ff2b44
Jim Whimpey <jimwhimpey@me.com>
2012-03-30 07:04:55 +1000

    Quoting someone in the forum doesn't overwrite what you might have already had in the textarea.

--- scripts/uqcc.js
+++ scripts/uqcc.js
@@ -5,6 +5,9 @@ $(function(){
 	// Handle quoting 
 	$(".quotes_link").click(function(){
 	
+		// Get the content that may already be in the textarea
+		var previousContent = $("textarea").val();
+	
 		// Get the HTML body of what we want to quote
 		var author = $(this).parents(".bbp-reply-header").next(".type-reply, .type-topic").find(".bbp-author-name").text();
 		var quoteContent = $(this).parents(".bbp-reply-header").next(".type-reply, .type-topic").find(".bbp-reply-content .content").html().trim();
@@ -14,7 +17,7 @@ $(function(){
 		quoteContent = "<cite>" + author + " said:</cite>\n<blockquote>" + quoteContent + "</blockquote>\n\n";
 		
 		// Put it into the text area surrounded by block quotes, make it bigger and put the cursor in there
-		$("textarea").css("height", "400px").focus().val(quoteContent).scrollTop(99999);
+		$("textarea").css("height", "400px").focus().val(previousContent + quoteContent);
 
 		// Scroll down to the reply field
 		$('html, body').animate({
